5ffa780e3a29356b3053dc267d5fd1ab1669709e
Lasse Wulff <46448799+benchmarkingbuffalo@users.noreply.github.com>
2025-05-04 12:31:54 +0200

    Initial setup for building the project

---
+++ src/main/java/io/github/benchmarkingbuffalo/springratelimiter/Example.java
@@ -0,0 +1,13 @@
+package io.github.benchmarkingbuffalo.springratelimiter;
+
+/**
+ * This is an example class to see, if the build is working.
+ */
+public class Example {
+
+    /**
+     * This is an example constructor to see, if the build is working.
+     */
+    public Example() {
+    }
+}
---
+++ src/test/java/io/github/benchmarkingbuffalo/springratelimiter/ExampleTest.java
@@ -0,0 +1,15 @@
+package io.github.benchmarkingbuffalo.springratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ExampleTest {
+
+    @Test
+    public void testExample() {
+        // This is an example test to see, if the build is working.
+        Example example = new Example();
+    }
+}
